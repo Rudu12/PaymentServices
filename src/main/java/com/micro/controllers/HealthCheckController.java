c48bd1312a21abe90109cbffb83aa41fdefef9f3
rudu12 <ruchiadubey@gmail.com>
2020-05-10 01:29:31 +0530

    Initial commit

---
+++ src/main/java/com/micro/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.micro.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@RequestMapping("/hc")
+	public String healthcheck() {
+		return "Everything Fine";
+	}
+}
--- src/main/java/com/micro/payments/PaymentServicesApplication.java
+++ src/main/java/com/micro/payments/PaymentServicesApplication.java
@@ -2,8 +2,10 @@ package com.micro.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.micro.controllers")
 public class PaymentServicesApplication {
 
 	public static void main(String[] args) {
