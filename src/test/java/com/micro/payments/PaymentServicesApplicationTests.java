75a2741a23b9506a2ba1b8e5e74c9c4912e22cc2
ruchi <ruchiadubey@gmail.com>
2020-05-10 00:49:36 +0530

    test

---
+++ src/test/java/com/micro/payments/PaymentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
